0fd15335bb5c4f7ee0562202c0b8e223af5bd04c
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-12-30 19:12:10 -0500

    styled logout button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { logout } from '../actions/usersAction'
+import {connect} from 'react-redux'
+import {logout} from '../actions/usersAction'
 
-const Logout = ({ logout, history}) => {
+const Logout = ({logout, history}) => {
 
   const handleOnSubmit = () => {
     logout(history)
     history.push('/');
   }
 
-  return (
-    <form onSubmit={handleOnSubmit}>
-<div className="two wide field">
+  return (<form onSubmit={handleOnSubmit}>
+    <div className="ui form">
+      <div className="two wide field">
 
-  <button type="submit" className="ui button">Log out</button>
-</div>
-    </form>
-  )
+        <button type="submit" className="ui button">Log out</button>
+      </div>
+    </div>
+  </form>)
 }
 
 export default connect(null, {logout})(Logout)
